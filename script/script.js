c92782a5d23cd1f8a67c6da00a8d0568e93764c0
Kapee87 <89433307+Kapee87@users.noreply.github.com>
2021-12-31 14:14:30 -0300

    nueva version

--- script/script.js
+++ script/script.js
@@ -5,5 +5,5 @@ const hamburger = document.querySelector(".hamburger");
 hamburger.addEventListener("click", function () {
     // Toggle class "is-active"
     hamburger.classList.toggle("is-active");
-    menu.classList.toggle('show')
+    menu.classList.toggle('show');
 });
